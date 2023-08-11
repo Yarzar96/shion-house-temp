31af89b86d0bd123719bc6ff8edbfd6e0c4a231e
Yarzar96 <yarzarsaw87@gmail.com>
2023-08-11 13:29:21 +0630

    M navbar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("Hello shion House")
+//start jquery area
+$(document).ready(function(){
+    $("#banner_carousel").owlCarousel({
+        items :1,
+        loop : true,
+        dots :false,
+        //autoplay :true
+    });
+})
+
+//end jquery area
